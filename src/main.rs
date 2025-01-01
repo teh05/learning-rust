c5e1455d26cef45f5e8962ec10948d8e7c6f9f3d
teh05 <achmad.ramadhan003@binus.ac.id>
2025-01-02 00:29:28 +0700

    bellajar letmut variabel

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,40 @@ fn main_test(){
     println!("Hello, world!");
     println!("Hello, world!");
 }
+
+
+#[test]
+fn nama_variables(){
+    let name = "mamadsss";
+    println!("Hello, {}!", name);
+}
+
+
+#[test]
+fn test_variabel(){
+    let name = "Achmad Rasyid Ramadhan";
+    println!("Hello, {}!", name);
+}
+
+
+#[test]
+fn test_mutable(){
+    let mut name = "Achmad Rasyid Ramadhan";
+    println!("Hello, {}!", name);
+
+    let name = "budi sadikin";
+    println!("Hello, {}!", name);
+}
+
+#[test]
+fn testttt_mamad(){
+    let mut name = "acmad rasyid Ramadhan";
+    println!("Hello, {}!", name);
+
+    let name = "budi kopling";
+    println!("Hello, {}!", name);
+}
+
+
+
+
